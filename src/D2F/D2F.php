5be4a7de2cd6772e8bf1d0bca46ea2f355f60057
Vitech Liu <liuzirui1122@163.com>
2019-01-24 01:00:31 +0800

    Update v0.0.3
    -Abandon file type option in json, use "/" for dir, "//" for symlink instead.
    -Dir Rouch-Match using weighted array similarity matching.

--- src/D2F/D2F.php
+++ src/D2F/D2F.php
@@ -4,11 +4,17 @@ namespace D2F;
 
 use D2F\Exception\InvalidDirException;
 
+define("DEFAULT_POWER",0.5);
+define("MINUMUM_POWER",1);
+
 final class D2F {
 
     public $dir;
     public $libs = [];
 
+    
+    
+    
     public function  __construct(){
         $this->dir = dirname(__FILE__).DIRECTORY_SEPARATOR."Library";
 
@@ -17,6 +23,43 @@ final class D2F {
         }
         
         print_r($this->layerSet($this->libs[0]["dir"],true));
+    }
+
+    public function analyze($dir) {
+        //First layer match
+        $firstLayer = $this->layerSet($dir);
+        $rec = [];
+        foreach($this->libs as $framework) {
+            if ($this->compareLayer($firstLayer,$key -> $framework) >= MINUMUM_POWER) $rec[] += $key;
+        }
+        if (count($rec) == 0) return ""; //空
+        if (count($rec) == 1) return $this->libs[$rec[0]]["name"]; //返回唯一的
+
+        return "WIP";
+
+    }
+
+    
+    private function compareLayer($layer,$framework) {
+        $match = 0;
+        $sum = 0;
+        foreach($framework["dir"] as $val) {
+            $sum ++;
+            $pow = DEFAULT_POWER;
+            if (is_array($val)) {
+                $pow = array_key_exists("power",$val) ? $val["power"] : DEFAULT_POWER;
+                $name = $val["name"];
+            } else {
+                $name = $val;
+            }
+            if (in_array($name,$layer)) {
+                $match += 1/$pow;
+            } else {
+                if ($pow == 1) return 0;
+            }
+            
+        }
+        return $match / $sum;
     }
 
     /**
@@ -32,33 +75,24 @@ final class D2F {
         }
     }
 
-    public function analyze($dir) {
-        
-    }
-
     /**
-     * Undocumented function
+     * Export one layer of input dir
      *
      * @param mixed $layer
-     * @param boolean $lib
      * @return array
      */
-    public function layerSet($layer,$lib = false) {
+    private function layerSet($layer) {
         if (!is_array($layer)) 
             throw new InvalidDirException("Input dir structure is not an array");
 
         $ans = [];
         foreach ($layer as $val) {
-            if (is_string($val) && ($lib || $this->validDirString($val))) {
+            if (is_string($val) && $this->validDirString($val)) {
                 $ans[] = trim($val);
-            } else if (is_array($val) && ($lib || $this->validDirArray($val))) {
-                $prefix = "";
-                if($val["type"] == "dir") $prefix = "/";
-                else if ($val["type"] == "symlink") $prefix = "//";
-                $ans[] = $prefix.trim($val["name"]);
+            } else if (is_array($val) && $this->validDirArray($val)) {
+                $ans[] = trim($val["name"]);
             }
         }   
-        //print_r($ans);
         return $ans;
     }
 
@@ -69,7 +103,7 @@ final class D2F {
      * @return boolean
      * @throws InvalidDirException
      */
-    function validDirString($str) {
+    private function validDirString($str) {
         if (strlen(trim($str)) == 0)
             throw new InvalidDirException("Input dir structure contains invalid file/dir");
 
@@ -83,18 +117,11 @@ final class D2F {
      * @return boolean
      * @throws InvalidDirException
      */
-    function validDirArray($arr) {
-        if (!array_key_exists("name",$arr) || !array_key_exists("type",$arr))
-            throw new InvalidDirException("Input dir structure contains invalid file/dir array");
-
-        if (!is_string($arr["name"]) || !is_string($arr["type"]))
+    private function validDirArray($arr) {
+        if (!array_key_exists("name",$arr) || !is_string($arr["name"]))
             throw new InvalidDirException("Input dir structure contains invalid file/dir array");
 
         $this->validDirString($arr["name"]);
-        $this->validDirString($arr["type"]);
-
-        if(!in_array($arr["type"],["file","dir","symlink"]))
-            throw new InvalidDirException("Input dir structure contains invalid 'type'");
 
         return true;
     }
